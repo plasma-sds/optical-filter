ceef59a460180124eb0c2574cae561981629ad2f
asztalosors <asztalos@reak.bme.hu>
2024-05-17 00:02:14 +0200

    #7 Correcting and ading definition to class

--- spectra/spectrum.py
+++ spectra/spectrum.py
@@ -5,3 +5,22 @@ Created on Thu May 16 18:22:30 2024
 @author: Örs
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Spectrum(object):
+    def __init__(self, wavelength, profile, 
+                 name='', label='Intensity', 
+                 unit='Ph/s/nm'):
+        
+        if len(wavelength) == len(profile):
+            self.wavelength = np.array(wavelength)
+            self.profile = np.array(profile)
+            self.name = name
+            self.label = label
+            self.unit = unit
+            
+    def show():
+        plt.plot(self.wavelength, self.profile)
+        plt.show()
