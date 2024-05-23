0f2b3eefca80ba3335ab5d9c94198e8ff00f2a3c
asztalosors <asztalos@reak.bme.hu>
2024-05-23 19:24:01 +0200

    #7 Adding setter function for interpolator.

--- spectra/spectrum.py
+++ spectra/spectrum.py
@@ -7,26 +7,35 @@ Created on Thu May 16 18:22:30 2024
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 
 
 class Spectrum(object):
-    def __init__(self, wavelength, profile, 
-                 name='', label='Intensity', 
+    def __init__(self, wavelength, profile, name='', label='Intensity', 
                  unit='Ph/s/nm'):
         
         if len(wavelength) != len(profile):
-            raise ValueError('The <wavelength> and <profile> input is required to of the same size.')
+            raise ValueError('The <wavelength> and <profile> input is' 
+                             'required to of the same size.')
         
         self.wavelength = np.array(wavelength)
         self.profile = np.array(profile)
         self.name = name
         self.label = label
         self.unit = unit
+        self.
+        
+    def _set_interpolator(self):
+        self.interpolate = interp1d(self.wavelength, self.profile, 
+                                    kind='quadratic', fill_value='extrapolate')        
             
     def show(self, xrange=None, yrange=None):
         plt.plot(self.wavelength, self.profile, 'r-', linewidth=3)
-        plt.title('Spectrum for: '+ self.name, )
+        plt.title('Spectrum for: '+ self.name, 
+                  fontweight = 'bold', fontsize=12)
         plt.xlabel('Wavelength [nm]', )
+        plt.ylabel(self.label + ' [' + self.unit + ']',
+                   fontweight = 'bold', fontsize=12)
         if xrange is None:
             plt.xlim([self.wavelength.min(), self.wavelength.max()])
         else:
@@ -35,4 +44,5 @@ class Spectrum(object):
             plt.ylim([self.profile(), self.profile.max()])
         else:
             plt.ylim(xrange)
+        plt.legend(fontsize=12, prop={'weight':'bold'}, edgecolor="inherit")
         plt.show()
