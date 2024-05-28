417f256362a6d2833ba5e2c21926b525ab038822
asztalosors <asztalos@reak.bme.hu>
2024-05-28 16:20:16 +0200

    #14 Adding testing.

--- spectra/spectrumtest.py
+++ spectra/spectrumtest.py
@@ -72,6 +72,22 @@ class MultiSpectrumTest(unittest.TestCase):
     INPUT_NAME = 'bes_spectrum'
     EXPECTED_ATTR = ['name', 'spectra']
     
+    INPUT_WAVELENGTH = numpy.array([6560, 6561, 6562, 6563, 6564, 6565, 
+                                    6566, 6567, 6568, 6569, 6570, 6571, 
+                                    6572, 6573, 6574, 6575, 6576, 6577, 
+                                    6578, 6579])
+    INPUT_PROFILES = numpy.array([6.52185956e+15, 7.50898104e+15, 
+                                  1.11625043e+16, 1.66442658e+16,
+                                  2.02841119e+16, 2.04521145e+16, 
+                                  2.14231774e+16, 2.88714057e+16,
+                                  4.09192675e+16, 4.75669193e+16, 
+                                  4.21983109e+16, 3.01976984e+16,
+                                  2.19027901e+16, 2.04041686e+16, 
+                                  2.04804396e+16, 1.72583136e+16,
+                                  1.17628952e+16, 7.77056599e+15, 
+                                  6.54854474e+15, 6.49523047e+15])
+    INPUT_NAMES = ['test', 'test x 2', 'test x 3']
+    
     def setUp(self):
         self.spectrum = MultiSpectrum(name=self.INPUT_NAME)
         
@@ -83,7 +99,8 @@ class MultiSpectrumTest(unittest.TestCase):
             assert hasattr(self.spectrum, attr)
     
     def test_spectrum_addition(self):
-        pass
+        self.spectrum.add_spectrum(self.INPUT_WAVELENGTH, self.INPUT_PROFILES, 
+                                   self.INPUT_NAMES[0])
     
     def test_spectrum_update(self):
         pass
