5de09ef00be8d4a0c2543cf2a4716a358af283dd
asztalosors <asztalos@reak.bme.hu>
2024-05-30 23:16:37 +0200

    #14 More debugging

--- spectra/spectrumtest.py
+++ spectra/spectrumtest.py
@@ -133,6 +133,6 @@ class MultiSpectrumTest(unittest.TestCase):
         self.spectrum.add_spectrum(self.INPUT_WAVELENGTH, self.INPUT_PROFILES, 
                                    self.INPUT_NAMES[1])
         actual_comp = self.spectrum.compare_spectra(self.INPUT_NAMES[0], self.INPUT_NAMES[1])
-        for actual, ref in zip(actual_comp, numpy.zeros(len(self.INPUT_PROFILES()))):
+        for actual, ref in zip(actual_comp, numpy.zeros(len(self.INPUT_PROFILES))):
             self.assertAlmostEqual(actual, ref, delta=100, msg='Spectral subtraction failed.')
         
