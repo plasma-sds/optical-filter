71cf820974995730334b034d39fedb0d9bac5965
asztalosors <asztalos@reak.bme.hu>
2024-06-05 15:32:43 +0200

    #11 Adding function.py with few basic empty functions.

---
+++ spectra/functions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  5 15:30:14 2024
+
+@author: Örs
+"""
+
+import numpy
+import scipy
+
+def calculate_sbr(input):
+    pass
+
+def apply_filter(spectrum, filter):
+    pass
+
+def calculate_filter_efficiency(spectrum):
+    pass
